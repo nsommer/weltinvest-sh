d9405fe95c35d602d9c84be07eb838241295d44e
Nils Sommer <mail@nilssommer.de>
2020-12-23 18:17:23 +0100

    Formatting

--- weltinvest.sh
+++ weltinvest.sh
@@ -14,8 +14,8 @@ RAISIN_PASSWORD=$2
 
 which jq > /dev/null
 if [ $? -ne 0 ]; then
-	echo "Please install jq (json processor), for example through homebrew."
-	exit
+  echo "Please install jq (json processor), for example through homebrew."
+  exit
 fi
 
 #
@@ -26,14 +26,14 @@ SESSION_COOKIE_FILENAME="/tmp/weltinvest_sh_session_cookies.txt"
 touch $SESSION_COOKIE_FILENAME
 
 LOGIN_RESPONSE=$(
-	curl \
-		https://banking.weltsparen.de/savingglobal/rest/open_api/v2/login \
-		--request POST \
-		--header 'Content-Type: application/json' \
-		--header 'Accept: application/json' \
-		--data "{ \"username\": \"$RAISIN_USERNAME\", \"password\": \"$RAISIN_PASSWORD\" }" \
-		--cookie-jar $SESSION_COOKIE_FILENAME \
-		--silent
+  curl \
+    https://banking.weltsparen.de/savingglobal/rest/open_api/v2/login \
+    --request POST \
+    --header 'Content-Type: application/json' \
+    --header 'Accept: application/json' \
+    --data "{ \"username\": \"$RAISIN_USERNAME\", \"password\": \"$RAISIN_PASSWORD\" }" \
+    --cookie-jar $SESSION_COOKIE_FILENAME \
+    --silent
 )
 
 DIPLAY_NAME=$(echo $LOGIN_RESPONSE | jq -r '.customer.display_name')
@@ -43,11 +43,11 @@ DIPLAY_NAME=$(echo $LOGIN_RESPONSE | jq -r '.customer.display_name')
 #
 
 INVESTMENT_RESPONSE=$(
-	curl \
-		https://banking.weltsparen.de/savingglobal/rest/open_api/v2/cockpit/investment \
-		--header 'Accept: application/json' \
-		--cookie $SESSION_COOKIE_FILENAME \
-		--silent
+  curl \
+    https://banking.weltsparen.de/savingglobal/rest/open_api/v2/cockpit/investment \
+    --header 'Accept: application/json' \
+    --cookie $SESSION_COOKIE_FILENAME \
+    --silent
 )
 	
 ASSET_BALANCE_AMOUNT=$(echo $INVESTMENT_RESPONSE | jq -r '.asset_balance.value')
@@ -64,9 +64,9 @@ echo "$PORTFOLIO_NAME: $ASSET_BALANCE_AMOUNT"
 #
 
 curl \
-	https://banking.weltsparen.de/savingglobal/rest/open_api/v2/logout \
-	--request POST \
-	--cookie $SESSION_COOKIE_FILENAME \
-	--silent
+  https://banking.weltsparen.de/savingglobal/rest/open_api/v2/logout \
+  --request POST \
+  --cookie $SESSION_COOKIE_FILENAME \
+  --silent
 
 rm $SESSION_COOKIE_FILENAME
